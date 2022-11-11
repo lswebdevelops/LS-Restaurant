620580cf2f875815dd8898d87a6313729c23d8cc
Luciano Sardanha <sardanhaluciano9690@gmail.com>
2022-11-11 18:57:38 -0300

    adding source maps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './kitchen.jpg';
+import Icon from './laughing.svg';
+
 
 
 function component() {
@@ -23,3 +24,27 @@ function component() {
   }
   
   document.body.appendChild(component());
+
+  function printMe() {
+    console.log('I get called from print.js!');
+  }
+  function component2() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
+    
+ 
+
+    return element;
+  }
+  
+  document.body.appendChild(component2());
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -20,8 +22,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+
       
     ],
   },
+
 
 };
