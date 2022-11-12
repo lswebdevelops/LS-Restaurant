b56cc7940baa2b5ef5d97943e4b8e17ac66b820d
Luciano Sardanha <sardanhaluciano9690@gmail.com>
2022-11-11 21:10:39 -0300

    adding some images

--- src/index.js
+++ src/index.js
@@ -1,50 +1,19 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './laughing.svg';
 
 
+const para = document.getElementById('content')
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['LS-Restaurant'], ' ');
-    element.classList.add('h1');
-
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-
-
-    return element;
-  }
-  
-  document.body.appendChild(component());
-
-  function printMe() {
-    console.log('I get called from print.js!');
-  }
-  function component2() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-  
-    element.appendChild(btn);
+  function paragh(){
     
- 
-
+	  const element =document.createElement('p');
+	  
+	   // Lodash, now imported by this script
+    element.innerHTML = _.join(['LS-RESTAURANT'], ' ');
+    element.classList.add('p1');
     return element;
+	
   }
   
-  document.body.appendChild(component2());
-
-
+   para.appendChild(paragh());
+	
